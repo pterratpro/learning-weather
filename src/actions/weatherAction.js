a48c43d3436b5adb97ab0b22f048ed239970bd93
pterratpro <pierreterrat.pro@gmail.com>
2020-02-19 10:23:39 +0100

    feat(weather): review weatherAction

--- src/actions/weatherAction.js
+++ src/actions/weatherAction.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 // Requete GET sur http://api.openweathermap.org/data/2.5/weather?q=London&appid=e14154b4f09fc841a46c2fd919707747
 //Axios pour faire ma requete
 
+const baseUrl = "http://api.openweathermap.org/data/2.5";
+const appId= "&appid=e14154b4f09fc841a46c2fd919707747";
 export function getWeather(){
-    return axios.get("http://api.openweathermap.org/data/2.5/weather?q=London&appid=e14154b4f09fc841a46c2fd919707747");
+    return axios.get(baseUrl+"/weather?q=London"+appId);
 }
