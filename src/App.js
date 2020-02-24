94cb87a871bd76e0c7d29b0edb79496f3544108a
pterratpro <pierreterrat.pro@gmail.com>
2020-02-24 17:10:21 +0100

    feat(global) : app design LITM academy

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Weather />
+      <header className="header">
+        <div className="logo">
+          <div className="logo-litm"></div>
+          <p className="logo-name">LITM Academy</p>
+        </div>
       </header>
+      <div className="body">
+        <Weather />
+      </div>
     </div>
   );
 }
